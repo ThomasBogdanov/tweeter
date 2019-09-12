1856cd2a08e326d7f9c1a76d9099a9f4597b28c9
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-09-12 21:39:38 +0000

    AJAX form submission implimented

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,33 +6,9 @@
 
 
 // Fake data taken from initial-tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
 const renderTweets = function(tweets) {
-
+  $('.tweetContainer').empty();
   tweets.forEach(data => {
       $('.tweetContainer').append(createTweetElement(data));
   });
@@ -67,5 +43,31 @@ const createTweetElement = function(data) {
 
 $(document).ready(function() {
   console.log("App.js ready!")
-  renderTweets(data);
+
+
+const formSubmission = $("#formSubmission");
+
+formSubmission.on("submit", (event) => {
+  event.preventDefault();
+
+  $.ajax({
+    url: '/tweets',
+    type: 'POST',
+    data: formSubmission.serialize()
+  })
+  .then(loadTweets);
+  $("#tweetInput").val('');
 });
+
+const loadTweets = function() {
+  $.ajax({ url: "/tweets"})
+    .then(tweets => {
+      renderTweets(tweets);
+    })
+  };
+  loadTweets();
+});
+
+
+//Paircoded with Anthony Zhu (thanks for helping me catch-up)
+//Code used from KV's lecture for AJAX for POST requests
