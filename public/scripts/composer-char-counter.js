cab4dc9477d39a580708e5757c3509432c069839
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-09-13 19:14:54 +0000

    removed a couple console logs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,9 +3,7 @@ $(document).ready(function() {
   console.log("composer ready!");
   $("#tweetInput").on('input', function() {
     let charCount = $(this).val().length;
-    console.log(charCount);
     let currentCount = maxCount - charCount;
-    console.log(currentCount);
     if (currentCount < 0) {
       $(".counter").css('color', 'red');
     } else {
